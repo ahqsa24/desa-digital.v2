b4570e1e4d342353b5941d62aafaa2e923a55e26
Adi Satria <adisatria@apps.ipb.ac.id>
2024-04-07 14:07:13 +0700

    add custom errors

---
+++ src/firebase/errors.ts
@@ -0,0 +1,7 @@
+export const FIREBASE_ERRORS = {
+    "Firebase: Error (auth/email-already-in-use).":
+      "Email sudah terdaftar",
+  
+    "Firebase: Error (auth/user-not-found).": "Email atau password salah",
+    "Firebase: Error (auth/wrong-password).": "Email atau password salah",
+  };
