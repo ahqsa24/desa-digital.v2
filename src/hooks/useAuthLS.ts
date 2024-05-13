9a12196d70b84e3e6c9fe91cc2e3fc4524fbac93
mochammadkevin <kevinariobimo@gmail.com>
2024-05-13 16:04:51 +0700

    refactor useAuthLS hook to use react-use's useLocalStorage

--- src/hooks/useAuthLS.ts
+++ src/hooks/useAuthLS.ts
@@ -1,52 +1,15 @@
-import { useState, useEffect } from 'react';
-import { ath, db } from '../firebase/clientApp';
-import { doc, getDoc } from 'firebase/firestore';
+import { useLocalStorage } from 'react-use'
 
 type Auth = {
-  id?: string;
-  email?: string;
-  role?: string;
+  id?: string
+  email?: string
+  role?: string
 }
 
 function useAuthLS() {
-  const [auth, setAuth] = useState<Auth>({});
+  const [auth, setAuth, remove] = useLocalStorage<Auth>('auth', {})
 
-  useEffect(() => {
-    const unsubscribe = ath.onAuthStateChanged(async (user) => {
-      if (user) {
-        console.log(`User is logged in with email: ${user.email}`);
-        try {
-          // Jika pengguna sudah login, dapatkan informasi peran pengguna dari Firestore
-          const userDoc = await getDoc(doc(db, 'users', user.uid));
-          if (userDoc.exists()) {
-            const userData = userDoc.data();
-            setAuth({
-              id: user.uid,
-              email: userData.email,
-              role: userData.role,
-            });
-          } else {
-            console.log('No such document!');
-          }
-        } catch (error) {
-          console.log('Error getting user document:', error);
-        }
-      } else {
-        // Jika pengguna tidak login, reset state auth
-        console.log('No user is logged in');
-        setAuth({});
-      }
-    });
-
-    return () => unsubscribe(); // Membersihkan subscription saat komponen unmount
-  }, []);
-
-  // Fungsi untuk logout menggunakan Firebase Authentication
-  const removeAuth = async () => {
-    await ath.signOut();
-  }
-
-  return { auth, setAuth, removeAuth };
+  return { auth, setAuth, removeAuth: remove }
 }
 
-export default useAuthLS;
+export default useAuthLS
