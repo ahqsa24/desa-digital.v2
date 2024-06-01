3f0a80130ad70565c45ef6f079f4eefada4a53ac
cheishaamanda <cheishaamandacheisha@apps.ipb.ac.id>
2024-06-01 14:40:14 +0700

    change burger icon

--- src/components/topBar/RightContent/UserMenu.tsx
+++ src/components/topBar/RightContent/UserMenu.tsx
@@ -1,38 +1,49 @@
-import { HamburgerIcon } from '@chakra-ui/icons';
-import { IconButton, Menu, MenuButton, MenuItem, MenuList } from '@chakra-ui/react';
-import { paths } from 'Consts/path';
-import { User, signOut } from 'firebase/auth';
-import React from 'react';
-import { useNavigate } from 'react-router';
-import { auth } from '../../../firebase/clientApp';
+import React from "react";
+import {
+  IconButton,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+} from "@chakra-ui/react";
+import { paths } from "Consts/path";
+import { User, signOut } from "firebase/auth";
+import { useNavigate } from "react-router";
+import { auth } from "../../../firebase/clientApp";
+import profileIcon from "Assets/icons/profile.svg";
 
 type UserMenuProps = {
-    user?: User | null;
+  user?: User | null;
 };
 
-const UserMenu:React.FC<UserMenuProps> = ({ user }) => {
-    const navigate = useNavigate();
+const UserMenu: React.FC<UserMenuProps> = ({ user }) => {
+  const navigate = useNavigate();
 
-    return (
-      <Menu>
-        <MenuButton
-          as={IconButton}
-          aria-label="Options"
-          icon={<HamburgerIcon />}
-        />
-        <MenuList>
-            {user ? (
-                <>
-                    <MenuItem onClick={() => signOut(auth)}>Logout</MenuItem>
-                </>
-            ) : (
-                <>
-                    <MenuItem onClick={() => navigate(paths.LOGIN_PAGE)}>Login</MenuItem>
-                </>
-            )}
-          
-        </MenuList>
-      </Menu>
-    );
-}
+  return (
+    <Menu>
+      <MenuButton
+        as={IconButton}
+        aria-label="Options"
+        icon={<img src={profileIcon} alt="Profile" width="24" height="24" />}
+      />
+      <MenuList>
+        {user ? (
+          <>
+            <MenuItem onClick={() => navigate(paths.INNOVATOR_PROFILE_PAGE)}>
+              Profile
+            </MenuItem>{" "}
+            {/* Tambahkan item Profile */}
+            <MenuItem onClick={() => signOut(auth)}>Logout</MenuItem>
+          </>
+        ) : (
+          <>
+            <MenuItem onClick={() => navigate(paths.LOGIN_PAGE)}>
+              Login
+            </MenuItem>
+          </>
+        )}
+      </MenuList>
+    </Menu>
+  );
+};
 export default UserMenu;
