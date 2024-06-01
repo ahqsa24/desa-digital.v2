f164d9f052b22922ab167bb40e9dce084c92c4d6
cheishaamanda <cheishaamandacheisha@apps.ipb.ac.id>
2024-06-01 13:18:25 +0700

    initial commit

--- src/pages/home/components/menu/index.tsx
+++ src/pages/home/components/menu/index.tsx
@@ -11,10 +11,37 @@ function Menu() {
   const navigate = useNavigate();
   const { data, isLoading, isFetched } = useQuery("menu", getCategories);
 
-  const [menu, setMenu] = useState<any>([]);
+  const predefinedCategories = [
+    {
+      icon: "https://github.com/fitrianurynt/PSBO/assets/79956203/9a53a458-b8bd-488c-82a6-17b0b2a86c0a",
+      title: "Pertanian Cerdas",
+    },
+    {
+      icon: "https://github.com/fitrianurynt/PSBO/assets/79956203/30cb9a40-cb83-4c3e-8950-cd6fd82a82db",
+      title: "Pemasaran Agri-Food dan E-Commerce",
+    },
+    {
+      icon: "https://github.com/fitrianurynt/PSBO/assets/79956203/8338ff39-61d6-4e78-895f-4f0577f699e1",
+      title: "E-Government",
+    },
+    {
+      icon: "https://github.com/cheishaamanda/capstone1/assets/90697739/e324ef2f-d3c9-443c-ad05-567676f6d76d",
+      title: "Sistem Informasi",
+    },
+    {
+      icon: "https://github.com/fitrianurynt/PSBO/assets/79956203/b4742250-5691-42ac-8a8a-6f9d5684b23b",
+      title: "Infrastruktur Lokal",
+    },
+    {
+      icon: "https://github.com/fitrianurynt/PSBO/assets/79956203/41581f29-9da8-417c-b24d-07bad3acd60b",
+      title: "Lihat Semua",
+    },
+  ];
+
+  const [menu, setMenu] = useState<any>(predefinedCategories);
 
   const onClick = (category: string) => {
-    if (category === "Semua") {
+    if (category === "Lihat Semua") {
       navigate(paths.INNOVATION_PAGE);
       return;
     }
@@ -25,11 +52,12 @@ function Menu() {
   };
 
   useEffect(() => {
-    if (isFetched) {
-      const temp = [...data?.slice(0, 5), data[data?.length - 1]];
-      setMenu(temp);
+    if (isFetched && data) {
+      //const temp = [...data?.slice(0, 5), data[data?.length - 1]];
+      //setMenu(temp);
+      setMenu(predefinedCategories);
     }
-  }, [isFetched]);
+  }, [isFetched, data]);
 
   return (
     <Container>
