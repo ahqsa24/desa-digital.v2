3baf8232f6aa8d270534b06d809a2cb3d6a12129
mochammadkevin <kevinariobimo@gmail.com>
2024-06-08 14:46:30 +0700

    Refactor form fields and imports in village profile page

--- src/pages/village/profile/index.tsx
+++ src/pages/village/profile/index.tsx
@@ -1,11 +1,16 @@
 import React, { useState, useEffect } from "react";
+import {
+  Button,
+  Flex,
+  Input,
+  Select,
+  Stack,
+  Text,
+  Textarea,
+} from "@chakra-ui/react";
 import TopBar from "Components/topBar";
 import Container from "Components/container";
-import TextField from "Components/textField";
-import Button from "Components/button";
 import { useForm } from "react-hook-form";
-import { Label } from "./_profileStyle";
-import Dropdown from "Components/dropDown";
 import { useMutation, useQuery } from "react-query";
 import { toast } from "react-toastify";
 import {
@@ -16,7 +21,6 @@ import {
 } from "Services/locationServices";
 import { updateProfile, getUserById } from "Services/userServices";
 import useAuthLS from "Hooks/useAuthLS";
-
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as z from "zod";
 
@@ -47,7 +51,6 @@ function AddVillage() {
     }
   );
 
-  // id
   const [selectedProvinsi, setSelectedProvinsi] = useState("");
   const [selectedKabupaten, setSelectedKabupaten] = useState("");
   const [selectedKecamatan, setSelectedKecamatan] = useState("");
@@ -72,7 +75,6 @@ function AddVillage() {
   const forms = [
     {
       label: "Provinsi",
-      type: "text",
       name: "province",
       placeholder: "Pilih provinsi",
       defaultValue: data?.province,
@@ -86,7 +88,6 @@ function AddVillage() {
     },
     {
       label: "Kabupaten/Kota",
-      type: "text",
       name: "district",
       placeholder: "Pilih kabupaten/kota",
       defaultValue: data?.district,
@@ -100,7 +101,6 @@ function AddVillage() {
     },
     {
       label: "Kecamatan",
-      type: "text",
       name: "subDistrict",
       placeholder: "Pilih kecamatan",
       defaultValue: data?.subDistrict,
@@ -114,10 +114,8 @@ function AddVillage() {
     },
     {
       label: "Desa/Kelurahan",
-      type: "text",
       name: "village",
       placeholder: "Pilih kelurahan",
-      value: data?.village,
       defaultValue: data?.village,
       isDisabled: data?.village,
       options:
@@ -128,38 +126,31 @@ function AddVillage() {
     },
     {
       label: "Nama Desa",
-      type: "text",
       name: "nameVillage",
       placeholder: "Nama desa",
     },
     {
       label: "Tentang Desa",
-      type: "text",
       name: "description",
       placeholder: "Masukan deskripsi desa",
     },
     {
       label: "Logo Desa",
-      type: "url",
       name: "logo",
       placeholder: "https://",
     },
     {
       label: "Header Desa",
-      type: "url",
       name: "header",
       placeholder: "https://",
     },
-
     {
       label: "Potensi Desa",
-      type: "text",
       name: "benefit",
       placeholder: "Masukkan potensi desa",
     },
     {
       label: "Nomor WhatsApp",
-      type: "tel",
       name: "whatsApp",
       placeholder: "62812345678",
     },
@@ -190,53 +181,83 @@ function AddVillage() {
     <Container page px={16}>
       <TopBar title="Profil Desa" />
       <form onSubmit={handleSubmit(onProfileSave)}>
-        {forms?.map(
-          (
-            {
-              label,
-              type,
-              name,
-              placeholder,
-              options,
-              onChange,
-              defaultValue,
-              isDisabled,
-            },
-            idx
-          ) => {
-            // dropdown
-            if (!!options)
-              return (
-                <React.Fragment key={idx}>
-                  <Label mt={12}>{label} </Label>
-                  <Dropdown
-                    form={form}
-                    name={name}
-                    options={options}
-                    onChange={onChange}
-                    placeholder={placeholder}
-                    defaultValue={defaultValue}
-                    isDisabled={isDisabled}
-                  />
-                </React.Fragment>
-              );
+        <Flex direction="column" marginTop="24px">
+          <Stack spacing={3} width="100%">
+            {forms.map(
+              (
+                {
+                  label,
+                  name,
+                  placeholder,
+                  options,
+                  onChange,
+                  defaultValue,
+                  isDisabled,
+                },
+                idx
+              ) => {
+                // dropdown
+                if (!!options)
+                  return (
+                    <React.Fragment key={idx}>
+                      <Text fontWeight="400" fontSize="14px">
+                        {label} <span style={{ color: "red" }}>*</span>
+                      </Text>
+                      <Select
+                        placeholder={placeholder}
+                        name={name}
+                        fontSize="10pt"
+                        variant="outline"
+                        cursor="pointer"
+                        color={"gray.500"}
+                        _focus={{
+                          outline: "none",
+                          bg: "white",
+                          border: "1px solid",
+                          borderColor: "black",
+                        }}
+                        _placeholder={{ color: "gray.500" }}
+                        defaultValue={defaultValue}
+                        onChange={(e) => onChange && onChange(e.target.value)}
+                        isDisabled={isDisabled}
+                      >
+                        {options.map((option: any) => (
+                          <option key={option.id} value={option.id}>
+                            {option.nama}
+                          </option>
+                        ))}
+                      </Select>
+                    </React.Fragment>
+                  );
 
-            return (
-              <React.Fragment key={idx}>
-                <Label mt={12}>{label} </Label>
-                <TextField
-                  mt={4}
-                  placeholder={placeholder || label}
-                  type={type}
-                  name={name}
-                  form={form}
-                />
-              </React.Fragment>
-            );
-          }
-        )}
-
-        <Button size="m" fullWidth mt={12} type="submit">
+                return (
+                  <React.Fragment key={idx}>
+                    <Text fontWeight="400" fontSize="14px">
+                      {label} <span style={{ color: "red" }}>*</span>
+                    </Text>
+                    <Input
+                      mt={4}
+                      placeholder={placeholder}
+                      name={name}
+                      fontSize="10pt"
+                      _placeholder={{ color: "gray.500" }}
+                      _focus={{
+                        outline: "none",
+                        bg: "white",
+                        border: "1px solid",
+                        borderColor: "black",
+                      }}
+                      defaultValue={defaultValue}
+                      isDisabled={isDisabled}
+                      {...form.register(name)}
+                    />
+                  </React.Fragment>
+                );
+              }
+            )}
+          </Stack>
+        </Flex>
+        <Button type="submit" mt="20px" width="100%" isLoading={form.formState.isSubmitting}>
           Simpan
         </Button>
       </form>
