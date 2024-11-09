68c2bbcfd26926c04cae20df49276866298db862
dinda <dindargsta@gmail.com>
2024-11-09 20:16:49 +0700

    add SearchBarInnov function to hero from components innovator

---
+++ src/pages/innovator/components/hero/SearchBarInnov.tsx
@@ -0,0 +1,40 @@
+import { SearchIcon } from "@chakra-ui/icons";
+import {
+    Flex,
+    Input,
+    InputGroup,
+    InputLeftElement
+} from "@chakra-ui/react";
+import React from "react";
+
+const SearchBarinnov: React.FC = () => {
+  return (
+    <Flex justify="center" maxW="360px" width="100%">
+        <InputGroup>
+          <InputLeftElement
+            pointerEvents="none"
+            children={<SearchIcon color="gray.300" />}
+          />
+          <Input
+            placeholder="Search"
+            fontSize="10pt"
+            _placeholder={{ color: "gray.500" }}
+            _hover={{
+              bg: "white",
+              border: "1px solid",
+              borderColor: "brand.100",
+            }}
+            _focus={{
+              bg: "white",
+              border: "1px solid",
+              borderColor: "#9CA3AF",
+            }}
+            borderRadius={100}
+            maxW="329px"
+            width="100%"
+          />
+        </InputGroup>
+      </Flex>
+  );
+};
+export default SearchBarinnov;
