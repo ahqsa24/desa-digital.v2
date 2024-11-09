43d354b027bab778f19434e9604b9e8f9e7cc6c2
jeki-z <hasbidzakiyyah@gmail.com>
2024-11-09 15:29:37 +0700

    fix position for content in desa digital

--- src/pages/village/index.tsx
+++ src/pages/village/index.tsx
@@ -2,11 +2,28 @@ import Hero from "./components/hero";
 import { useQuery } from "react-query";
 import { getUsers } from "Services/userServices";
 import { useNavigate, generatePath } from "react-router-dom";
-import { GridContainer, CardContent, Containers } from "./_villageStyle";
+import { GridContainer, 
+          CardContent, 
+          Containers, 
+          Text, 
+          Texthighlight,
+          Column} from "./_villageStyle";
 import CardVillage from "Components/card/village";
 import { paths } from "Consts/path";
 import Container from "Components/container";
-import { Box, Flex, Text } from "@chakra-ui/react";
+import {Box, Select} from "@chakra-ui/react";
+
+const categories = [
+  "E-Government",
+  "E-Tourism",
+  "Layanan Keuangan",
+  "Layanan Sosial",
+  "Pemasaran Agri-Food dan E-Commerce",
+  "Pengembangan Masyarakat dan Ekonomi",
+  "Pengelolaan Sumber Daya",
+  "Pertanian Cerdas",
+  "Sistem Informasi",
+];
 
 function Village() {
   const navigate = useNavigate();
@@ -17,8 +34,37 @@ function Village() {
   <Box>
       <Hero />
       <Containers> 
-        <Text mt= "95px">aa</Text>
-        <CardContent/>
+        <CardContent> 
+          <Column>
+          <Text>
+              Pilih Provinsi
+            </Text>
+            <Select
+              placeholder="Pilih Provinsi"
+              name="category"
+              fontSize="10pt"
+              variant="outline"
+              cursor="pointer"
+              color={"gray.500"}
+              _focus={{
+                outline: "none",
+                bg: "white",
+                border: "1px solid",
+                borderColor: "#E5E7EB",
+              }}
+              //value={category}
+              //onChange={onSelectCategory}
+            >
+              {categories.map((category) => (
+                <option key={category} value={category}>
+                  {category}
+                </option>
+              ))}
+            </Select>
+          </Column>
+          <Column>b</Column>
+        </CardContent>
+        <Text> Menampilkan 8 desa untuk <Texthighlight>"Semua Provinsi"</Texthighlight> </Text>
         <GridContainer>
           {isFetched &&
             villages?.map((item: any, idx: number) => (
@@ -34,9 +80,7 @@ function Village() {
             ))}
         </GridContainer>
       </Containers>
-
-      
-  </Box>
+    </Box>
   );
 }
 
