b1ebd9072592b66541f3dba09d123d99aee71dc4
dinda <dindargsta@gmail.com>
2024-11-11 21:57:44 +0700

    add toast tyle for "login berhasil" and add import ButtonHTMLAttributes

--- src/pages/home/_homeStyle.ts
+++ src/pages/home/_homeStyle.ts
@@ -1,7 +1,8 @@
 import Button from 'Components/button';
 import styled from 'styled-components';
+import { ButtonHTMLAttributes } from "react";
 
-export const FloatingButton = styled(Button)`
+export const FloatingButton = styled(Button)<ButtonHTMLAttributes<HTMLButtonElement>>`
   border-radius: 50%;
   height: 60px;
   width: 60px;
@@ -16,4 +17,13 @@ export const FloatingButton = styled(Button)`
   margin-right: 19px;
 `;
 
+export const Toast = styled.div`
+  color: #347357; /* Warna hijau */
+  padding: 8px; /* p={2}, diubah ke piksel */
+  background-color: #FFF; /* Warna putih */
+  border-radius: 4px; /* md diubah ke nilai piksel */
+  font-family: 'Inter', sans-serif; /* Font Inter */
+  font-size: 14px; /* Ukuran font */
+  box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1); /* lg diubah ke nilai eksplisit */
+`;
 
