94de25e355ca66e7a1497cc09ff7d4f3e3eb2a72
dinda <dindargsta@gmail.com>
2024-11-11 13:21:18 +0700

    change the size of height, eidth, and margin-right styled button

--- src/pages/home/_homeStyle.ts
+++ src/pages/home/_homeStyle.ts
@@ -1,10 +1,10 @@
-import Button from 'Components/button'
-import styled from 'styled-components'
+import Button from 'Components/button';
+import styled from 'styled-components';
 
 export const FloatingButton = styled(Button)`
   border-radius: 50%;
-  height: 50px;
-  width: 50px;
+  height: 60px;
+  width: 60px;
   padding: 0;
   display: flex;
   align-items: center;
@@ -13,5 +13,7 @@ export const FloatingButton = styled(Button)`
   z-index: 999;
   bottom: 70px;
   margin-left: auto;
-  margin-right: 10px;
-`
+  margin-right: 19px;
+`;
+
+
