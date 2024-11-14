833ecec7b9096118e6be37d981681275d871aa8d
dinda <dindargsta@gmail.com>
2024-11-15 01:24:55 +0700

    delete style of floatingbutton

--- src/pages/home/_homeStyle.ts
+++ src/pages/home/_homeStyle.ts
@@ -1,21 +1,4 @@
-import Button from 'Components/button';
 import styled from 'styled-components';
-import { ButtonHTMLAttributes } from "react";
-
-export const FloatingButton = styled(Button)<ButtonHTMLAttributes<HTMLButtonElement>>`
-  border-radius: 50%;
-  height: 60px;
-  width: 60px;
-  padding: 0;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  position: sticky;
-  z-index: 999;
-  bottom: 70px;
-  margin-left: auto;
-  margin-right: 19px;
-`;
 
 export const Toast = styled.div`
   color: #347357; /* Warna hijau */
