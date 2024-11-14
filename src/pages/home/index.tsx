fb1b98a7d443e794d52febae6e555ba5671d6c15
dinda <dindargsta@gmail.com>
2024-11-15 01:25:22 +0700

    add tooltip

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,6 @@
 import { useToast } from '@chakra-ui/toast';
-import Add from "Assets/icons/add.svg";
-import Container from "Components/container";
-import TopBar from "Components/topBar/TopBar";
-import { paths } from "Consts/path";
-import { getAuth, onAuthStateChanged } from "firebase/auth"; // import firebase auth
+import { Box, Button, IconButton, Tooltip } from '@chakra-ui/react';
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 import {
   collection,
   getFirestore,
@@ -15,13 +12,13 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { FloatingButton } from "./_homeStyle";
+import { AddIcon } from '@chakra-ui/icons';
+import Container from "Components/container";
+import TopBar from "Components/topBar/TopBar";
+import { paths } from "Consts/path";
 import Hero from "./components/hero";
-import Innovator from "./components/innovator";
 import Menu from "./components/menu";
 import Readiness from "./components/readiness";
-import { Toast } from './_homeStyle';
-
 
 function Home() {
   const navigate = useNavigate();
@@ -33,25 +30,33 @@ function Home() {
   useEffect(() => {
     const hasLoggedIn = localStorage.getItem("hasLoggedIn");
 
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
+    const unsubscribe = onAuthStateChanged(auth, async (user) => {
       if (user) {
         if (!hasLoggedIn) {
           toast({
             title: "Anda berhasil login!",
             status: "success",
-            duration: 1000,
+            duration: 3000,
             isClosable: true,
             position: "top",
             render: () => (
-              <Toast>
+              <Box
+                color="#16A34A"
+                p={2}
+                bg="#FFF"
+                borderRadius="md"
+                fontFamily="Inter"
+                fontSize="14px"
+                boxShadow="lg"
+              >
                 Anda berhasil login!
-              </Toast>
+              </Box>
             ),
           });
           localStorage.setItem("hasLoggedIn", "true");
         }
 
-        user.getIdToken().then((token) => {
+        try {
           const db = getFirestore();
           const colRef = collection(db, "users");
           const q = query(colRef, where("id", "==", user.uid));
@@ -66,13 +71,11 @@ function Home() {
           const innovatorsRef = collection(db, "innovators");
           const qInnovators = query(innovatorsRef, where("id", "==", user.uid));
           onSnapshot(qInnovators, (snapshot) => {
-            if (!snapshot.empty) {
-              setIsInnovator(true);
-            } else {
-              setIsInnovator(false);
-            }
+            setIsInnovator(!snapshot.empty);
           });
-        });
+        } catch (error) {
+          console.error("Error fetching user data or token", error);
+        }
       } else {
         setUserRole(null);
         setIsInnovator(false);
@@ -90,8 +93,8 @@ function Home() {
       toast({
         title: "Lengkapi Profil terlebih dahulu",
         status: "error",
-        duration: 1000,
-        isClosable: false,
+        duration: 3000,
+        isClosable: true,
         position: "top",
       });
     }
@@ -103,11 +106,36 @@ function Home() {
       <Hero />
       <Menu />
       <Readiness />
-      <Innovator />
       {userRole === "innovator" && (
-        <FloatingButton onClick={handleAddInnovationClick}>
-          <img src={Add} width={20} height={20} alt="add icon" />
-        </FloatingButton>
+        <Tooltip
+          label="Tambah Inovasi"
+          aria-label="Tambah Inovasi Tooltip"
+          placement="top"
+          hasArrow
+          bg="#347357"
+          color="white"
+          fontSize="12px"
+          p={1}
+          borderRadius='10'
+        >
+          <Button 
+            borderRadius='50%' 
+            width='60px' 
+            height='60px' 
+            padding='0' 
+            display='flex' 
+            alignItems='center' 
+            justifyContent='center' 
+            position='sticky' 
+            zIndex='999' 
+            bottom='68px' 
+            marginLeft='auto' 
+            marginRight='33px'
+            onClick={handleAddInnovationClick}
+          >
+            <IconButton icon={<AddIcon />} aria-label="Tambah Inovasi" />
+          </Button>
+        </Tooltip>
       )}
       <ToastContainer />
     </Container>
