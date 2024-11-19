85743ba72619e94a5ba2a5da8c93443e059db3fa
Adi Satria <adisatria@apps.ipb.ac.id>
2024-11-19 08:28:27 +0700

    Add BestBanner component and integrate it into AdminPage

--- src/pages/admin/AdminPage.tsx
+++ src/pages/admin/AdminPage.tsx
@@ -4,13 +4,13 @@ import React from "react";
 import Hero from "../home/components/hero";
 import SearchBar from "Components/search/SearchBar";
 import Menu from "../home/components/menu";
-import { Flex, Stack } from "@chakra-ui/react";
+import { Box, Flex, Stack } from "@chakra-ui/react";
 import Rediness from "Components/rediness/Rediness";
 import Ads from "Components/ads/Ads";
+import Innovator from "../home/components/innovator";
+import BestBanner from "Components/banner/BestBanner";
 
-type AdminPageProps = {};
-
-const AdminPage: React.FC<AdminPageProps> = () => {
+const AdminPage: React.FC = () => {
   return (
     <Container page>
       <TopBar title="Desa Digital Indonesia" />
@@ -19,13 +19,17 @@ const AdminPage: React.FC<AdminPageProps> = () => {
         text="Digital Indonesia"
         isAdmin={true}
       />
-      <Stack direction="column">
+      <Stack direction="column" gap={2}>
         <SearchBar />
         <Menu isAdmin={true} />
-        <Flex direction="row" justifyContent='space-between' padding='0 14px'>
+        <Flex direction="row" justifyContent="space-between" padding="0 14px">
           <Rediness />
           <Ads />
         </Flex>
+        <BestBanner />
+        <Box mt="120px">
+          <Innovator />
+        </Box>
       </Stack>
     </Container>
   );
