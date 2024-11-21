b383b2797b4ae8f896831bb6ae70a159639c35f0
Adi Satria <adisatria@apps.ipb.ac.id>
2024-11-21 20:17:10 +0700

    Refactor UserMenu and TopBar components: enhance layout with Flex, add logout functionality, and conditionally render UserMenu based on route

--- src/components/topBar/index.tsx
+++ src/components/topBar/index.tsx
@@ -2,7 +2,7 @@ import { ArrowBackIcon } from "@chakra-ui/icons";
 import { Box, Button, Flex, Text } from "@chakra-ui/react";
 import { paths } from "Consts/path";
 import { useAuthState } from "react-firebase-hooks/auth";
-import { useNavigate } from "react-router";
+import { useLocation, useNavigate } from "react-router";
 import { auth } from "../../firebase/clientApp";
 import UserMenu from "./RightContent/UserMenu";
 
@@ -14,8 +14,12 @@ type TopBarProps = {
 function TopBar(props: TopBarProps) {
   const { title, onBack } = props;
   const navigate = useNavigate();
+  const location = useLocation();
 
   const [user] = useAuthState(auth);
+
+  const allowedPaths = [paths.LANDING_PAGE, paths.ADMIN_PAGE];
+  const isUserMenuVisible = allowedPaths.includes(location.pathname);
 
   return (
     <Box
@@ -27,27 +31,42 @@ function TopBar(props: TopBarProps) {
       width="100%"
       height="56px"
       zIndex="999"
-      alignContent='center'
+      alignContent="center"
     >
-      <Flex justify="space-between" align="center">
-        {!!onBack && <ArrowBackIcon color='white' fontSize='14pt' cursor="pointer" onClick={onBack} />}
-        <Text fontSize="16px" fontWeight="700" color="white">
+      <Flex
+        justify={isUserMenuVisible ? `space-between` : `flex-start`}
+        align="center"
+      >
+        {!!onBack && (
+          <ArrowBackIcon
+            color="white"
+            fontSize="14pt"
+            cursor="pointer"
+            onClick={onBack}
+          />
+        )}
+        <Text
+          fontSize="16px"
+          fontWeight="700"
+          color="white"
+          ml={isUserMenuVisible ? `0` : `24px`}
+        >
           {title}
         </Text>
-        {user ? (
-          <UserMenu user={user} />
-        ) : (
-          <Button
-            fontSize="14px"
-            fontWeight="700"
-            color="white"
-            cursor="pointer"
-            onClick={() => navigate(paths.LOGIN_PAGE)}
-            variant="link"
-          >
-            Login
-          </Button>
-        )}
+        {user
+          ? isUserMenuVisible && <UserMenu user={user} />
+          : isUserMenuVisible && (
+              <Button
+                fontSize="14px"
+                fontWeight="700"
+                color="white"
+                cursor="pointer"
+                onClick={() => navigate(paths.LOGIN_PAGE)}
+                variant="link"
+              >
+                Login
+              </Button>
+            )}
       </Flex>
     </Box>
   );
