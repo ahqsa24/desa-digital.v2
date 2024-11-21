760e169743f85f236f0d291c163057da8f91abda
dinda <dindargsta@gmail.com>
2024-11-21 17:20:25 +0700

    add new style for background innovator header

--- src/pages/innovator/components/hero/_heroStyle.ts
+++ src/pages/innovator/components/hero/_heroStyle.ts
@@ -1,10 +1,13 @@
 import styled from 'styled-components'
-import HeaderInnovator from 'Assets/images/header-innovator.jpg'
+import HeaderInnovator from 'Assets/images/header-innovator-txt.svg'
 
 export const Background = styled.div`
   background-image: url(${HeaderInnovator});
   background-size: cover;
-  background-position: center;
   background-repeat: no-repeat;
-  height: 162px;
+  background-position:left top;
+  display: flex;
+  width: 100%;
+  height: 145px;
+  position: relative;
 `
