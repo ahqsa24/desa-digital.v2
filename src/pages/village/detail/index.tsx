6e1887580024481443cff32fc855e1916a4afd5d
jeki-z <hasbidzakiyyah@gmail.com>
2024-11-22 17:13:34 +0700

    finishing detail village

--- src/pages/village/detail/index.tsx
+++ src/pages/village/detail/index.tsx
@@ -1,17 +1,32 @@
 import React from "react";
 import TopBar from "Components/topBar";
-import { useNavigate, useParams } from "react-router";
+import { useParams } from "react-router";
 import Button from "Components/button";
 import Location from "Assets/icons/location.svg";
 import Whatsapp from "Assets/icons/whatsapp.svg";
 import Instagram from "Assets/icons/instagram.svg";
 import Web from "Assets/icons/web.svg";
 import Geography from "Assets/icons/geography.svg";
+import Infrastructure from "Assets/icons/infrastructure.svg";
+import DigitalRead from "Assets/icons/digital-readiness.svg";
+import DigitalLit from "Assets/icons/digital-literacy.svg";
+import GoodService from "Assets/icons/good-service.svg";
+import SocCul from "Assets/icons/socio-cultural.svg";
+import Resource from "Assets/icons/resource-village.svg";
+import Efishery from "Assets/images/efishery.jpg";
+import EnlargedImage from "../components/Image";
+import CardInnovation from "Components/card/innovation";
+import { useEffect, useState } from "react";
+import { paths } from "Consts/path";
+import { getUserById } from "Services/userServices";
+import { useQuery } from "react-query";
+
 import {
   Title,
   ActionContainer,
   Icon,
-  Text,
+  SubText,
+  Text2,
   Logo,
   Label,
   Description,
@@ -19,7 +34,9 @@ import {
   ChipContainer,
   Background,
   ContPotensiDesa,
-  ButtonKontak
+  ButtonKontak,
+  CardContainer,
+  Horizontal,
 } from "./_detailStyle";
 import {
   Accordion,
@@ -28,12 +45,8 @@ import {
   AccordionPanel,
   AccordionIcon,
   Flex,
-  UnorderedList,
-  ListItem,
+  Text
 } from '@chakra-ui/react'
-import { paths } from "Consts/path";
-import { getUserById } from "Services/userServices";
-import { useQuery } from "react-query";
 import {
   Drawer,
   DrawerBody,
@@ -46,12 +59,28 @@ import {
   DrawerFooter,
   Box,
 } from '@chakra-ui/react'
+import { firestore } from "../../../firebase/clientApp";
+import Container from "Components/container";
+import { DocumentData, collection, getDocs, query, where } from "firebase/firestore";
+import { generatePath, useNavigate } from "react-router-dom";
+
+
 
 export default function DetailVillage() {
   const navigate = useNavigate();
+  const innovationsRef = collection(firestore, "innovations");
+  const [innovations, setInnovations] = useState<DocumentData[]>([]);
   const { id } = useParams();
-
   const { isOpen, onOpen, onClose } = useDisclosure()
+
+  useEffect(() => {
+    const fetchInnovations = async () => {
+      const innovationsSnapshot = await getDocs(innovationsRef);
+      const innovationsData = innovationsSnapshot.docs.map((doc) => doc.data());
+      setInnovations(innovationsData);
+    };
+    fetchInnovations();
+  }, [firestore]);
 
   const { data, isLoading } = useQuery<any>("villageById", () =>
     getUserById(id)
@@ -91,11 +120,11 @@ export default function DetailVillage() {
             </Description>
           </ActionContainer>
           <div>
-            <Text margin-bottom={16}>Tentang</Text>
+            <SubText margin-bottom={16}>Tentang</SubText>
             <Description>{description}</Description>
           </div>
           <div>
-            <Text>Potensi Desa</Text>
+            <SubText>Potensi Desa</SubText>
             <ContPotensiDesa>
               <ChipContainer>
                 <Label>{benefit}</Label>
@@ -108,45 +137,153 @@ export default function DetailVillage() {
               </ChipContainer>
             </ContPotensiDesa>
           </div>
-          <Accordion defaultIndex={[0]} allowMultiple>
-            <AccordionItem>
-              <h2>
-                <AccordionButton>
-                  <Flex as='span' flex='1' textAlign='left' fontSize="12px" fontWeight="700" gap={2}>
-                    <Icon src={Geography} alt="geo" /> Geografis
-                  </Flex>
-                  <AccordionIcon color="#347357" />
-                </AccordionButton>
-              </h2>
-              <AccordionPanel pb={4} fontSize={12}>
-                Dataran rendah seluas 4,50 Km
-              </AccordionPanel>
-            </AccordionItem>
-
-            <AccordionItem>
-              <h2>
-                <AccordionButton>
-                  <Box as='span' flex='1' textAlign='left'>
-                    Section 2 title
-                  </Box>
-                  <AccordionIcon />
-                </AccordionButton>
-              </h2>
-              <AccordionPanel pb={4}>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                commodo consequat.
-              </AccordionPanel>
-            </AccordionItem>
-          </Accordion>
+          <div>
+            <SubText>Karakteristik Desa</SubText>
+            <Accordion defaultIndex={[0]} allowMultiple>
+              <AccordionItem>
+                <h2>
+                  <AccordionButton paddingLeft="4px" paddingRight="4px">
+                    <Flex as='span' flex='1' textAlign='left' fontSize="12px" fontWeight="700" gap={2}>
+                      <Icon src={Geography} alt="geo" /> Geografis
+                    </Flex>
+                    <AccordionIcon color="#347357" />
+                  </AccordionButton>
+                </h2>
+                <AccordionPanel pb={4} fontSize={12} paddingLeft="4px" paddingRight="4px">
+                  Dataran rendah seluas 4,50 Km
+                </AccordionPanel>
+              </AccordionItem>
+              <AccordionItem>
+                <h2>
+                  <AccordionButton paddingLeft="4px" paddingRight="4px">
+                    <Flex as='span' flex='1' textAlign='left' fontSize="12px" fontWeight="700" gap={2}>
+                      <Icon src={Infrastructure} alt="Infrastrusture" /> Infrastruktur
+                    </Flex>
+                    <AccordionIcon color="#347357" />
+                  </AccordionButton>
+                </h2>
+                <AccordionPanel pb={4} fontSize={12} paddingLeft="4px" paddingRight="4px">
+                  Dataran rendah seluas 4,50 Km
+                </AccordionPanel>
+              </AccordionItem>
+              <AccordionItem>
+                <h2>
+                  <AccordionButton paddingLeft="4px" paddingRight="4px">
+                    <Flex as='span' flex='1' textAlign='left' fontSize="12px" fontWeight="700" gap={2}>
+                      <Icon src={DigitalRead} alt="DigR" /> Kesiapan Digital
+                    </Flex>
+                    <AccordionIcon color="#347357" />
+                  </AccordionButton>
+                </h2>
+                <AccordionPanel pb={4} fontSize={12} paddingLeft="4px" paddingRight="4px">
+                  Dataran rendah seluas 4,50 Km
+                </AccordionPanel>
+              </AccordionItem>
+              <AccordionItem>
+                <h2>
+                  <AccordionButton paddingLeft="4px" paddingRight="4px">
+                    <Flex as='span' flex='1' textAlign='left' fontSize="12px" fontWeight="700" gap={2}>
+                      <Icon src={DigitalLit} alt="DigL" /> Literasi Digital
+                    </Flex>
+                    <AccordionIcon color="#347357" />
+                  </AccordionButton>
+                </h2>
+                <AccordionPanel pb={4} fontSize={12} paddingLeft="4px" paddingRight="4px">
+                  Dataran rendah seluas 4,50 Km
+                </AccordionPanel>
+              </AccordionItem>
+              <AccordionItem>
+                <h2>
+                  <AccordionButton paddingLeft="4px" paddingRight="4px">
+                    <Flex as='span' flex='1' textAlign='left' fontSize="12px" fontWeight="700" gap={2}>
+                      <Icon src={GoodService} alt="GoodService" /> Pemantapan Pelayanan
+                    </Flex>
+                    <AccordionIcon color="#347357" />
+                  </AccordionButton>
+                </h2>
+                <AccordionPanel pb={4} fontSize={12} paddingLeft="4px" paddingRight="4px">
+                  Dataran rendah seluas 4,50 Km
+                </AccordionPanel>
+              </AccordionItem>
+              <AccordionItem>
+                <h2>
+                  <AccordionButton paddingLeft="4px" paddingRight="4px">
+                    <Flex as='span' flex='1' textAlign='left' fontSize="12px" fontWeight="700" gap={2}>
+                      <Icon src={SocCul} alt="SocCul" /> Sosial dan Budaya
+                    </Flex>
+                    <AccordionIcon color="#347357" />
+                  </AccordionButton>
+                </h2>
+                <AccordionPanel pb={4} fontSize={12} paddingLeft="4px" paddingRight="4px">
+                  Dataran rendah seluas 4,50 Km
+                </AccordionPanel>
+              </AccordionItem>
+              <AccordionItem>
+                <h2>
+                  <AccordionButton paddingLeft="4px" paddingRight="4px">
+                    <Flex as='span' flex='1' textAlign='left' fontSize="12px" fontWeight="700" gap={2}>
+                      <Icon src={Resource} alt="Resource" /> Sumber Daya Alam
+                    </Flex>
+                    <AccordionIcon color="#347357" />
+                  </AccordionButton>
+                </h2>
+                <AccordionPanel pb={4} fontSize={12} paddingLeft="4px" paddingRight="4px">
+                  Dataran rendah seluas 4,50 Km
+                </AccordionPanel>
+              </AccordionItem>
+            </Accordion>
+          </div>
+          <div>
+            <SubText>Galeri Desa</SubText>
+            <CardContainer>
+              <Horizontal>
+                <EnlargedImage src={Efishery} />
+                <EnlargedImage src={Efishery} />
+                <EnlargedImage src={Efishery} />
+                <EnlargedImage src={Efishery} />
+              </Horizontal>
+            </CardContainer>
+          </div>
+          <div>
+            <Flex justifyContent="space-between" alignItems="flex-end" align-self="stretch">
+              <SubText>Inovasi yang Diterapkan</SubText>
+              <Text
+                onClick={() => navigate("/target-page")} // Ganti "/target-page" dengan rute yang sesuai
+                cursor="pointer"
+                color="var(--Primary, #347357)"
+                fontSize="12px"
+                fontWeight="500"
+                textDecorationLine="underline"
+                paddingBottom="12px"
+              > Lihat Semua </Text>
+            </Flex>
+            <CardContainer>
+              <Horizontal>
+                {innovations.map((innovation, idx) => (
+                  <CardInnovation
+                    key={idx}
+                    images={innovation.images}
+                    namaInovasi={innovation.namaInovasi}
+                    kategori={innovation.kategori}
+                    deskripsi={innovation.deskripsi}
+                    tahunDibuat={innovation.tahunDibuat}
+                    innovatorLogo={innovation.innovatorImgURL}
+                    innovatorName={innovation.namaInnovator}
+                    onClick={() =>
+                      navigate(generatePath(paths.DETAIL_INNOVATION_PAGE, { id: innovation.id }))
+                    }
+                  />
+                ))}
+              </Horizontal>
+            </CardContainer>
+          </div>
 
           <Button size="m" fullWidth mb={70} type="submit" onClick={onOpen}>
             Kontak Desa
           </Button>{" "}
 
         </ContentContainer>
-      </div>
+      </div >
       <Drawer
         isOpen={isOpen}
         placement='bottom'
@@ -192,7 +329,7 @@ export default function DetailVillage() {
           </DrawerFooter>
         </DrawerContent>
       </Drawer>
-    </Box>
+    </Box >
   );
 }
 
