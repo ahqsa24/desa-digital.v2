5333e44f2ef5ab793bb7ea13949d95b1cf994cc3
Adi Satria <adisatria@apps.ipb.ac.id>
2024-11-26 09:40:36 +0700

    Refactor CardNotification component and update VerificationPage: enhance notification display with dynamic status and description handling, and remove unused ListNotification component.

--- src/components/card/notification/CardNotification.tsx
+++ src/components/card/notification/CardNotification.tsx
@@ -1,154 +1,102 @@
-import { Flex, Stack, Text } from "@chakra-ui/react";
-import React from "react";
+import { Button, Flex, Stack, Text } from "@chakra-ui/react";
+import React, { useState } from "react";
 
-type CardNotificationProps = {};
+type CardNotificationProps = {
+  title: string;
+  status: string;
+  date: string;
+  description: string;
+};
 // TODO: Buat agar card ini bisa di klik dan arahkan ke halaman detail
 
-const CardNotification: React.FC<CardNotificationProps> = () => {
+const CardNotification: React.FC<CardNotificationProps> = ({
+  title,
+  description,
+  status,
+  date,
+}) => {
+
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const getShortDescription = (text: string, wordLimit: number) => {
+    const words = text.split(" ");
+    if (words.length <= wordLimit) return text;
+    return words.slice(0, wordLimit).join(" ") + "...";
+  };
+
   return (
-    <Stack padding="0 16px" gap={2}>
-      <Flex
-        padding="12px"
-        borderRadius="8px"
-        border="1px solid"
-        borderColor="gray.300"
-        direction="column"
-        boxShadow="0px 1px 2px 0px rgba(0, 0, 0, 0.06), 0px 1px 3px 0px rgba(0, 0, 0, 0.10);"
-        cursor="pointer"
-      >
-        <Flex justifyContent="space-between">
-          <Text
-            fontSize="14px"
-            fontWeight="700"
-            lineHeight="140%"
-            width="221px"
-          >
-            Desa Soge
-          </Text>
-          <Flex
-            justifyContent="center"
-            alignItems="center"
-            width="79px"
-            height="18px"
-            borderRadius="12px"
-            border="0.5px solid"
-            borderColor="#EAB308"
-            background="#FEF9C3"
-            fontSize="10px"
-            fontWeight="400"
-            color="#CA8A04"
-          >
-            Menunggu
-          </Flex>
-        </Flex>
-        <Text
-          fontSize="12px"
+    <Flex
+      padding="12px"
+      borderRadius="8px"
+      border="1px solid"
+      borderColor="gray.300"
+      direction="column"
+      boxShadow="0px 1px 2px 0px rgba(0, 0, 0, 0.06), 0px 1px 3px 0px rgba(0, 0, 0, 0.10);"
+      cursor="pointer"
+    >
+      <Flex justifyContent="space-between">
+        <Text fontSize="14px" fontWeight="700" lineHeight="140%" width="221px">
+          {title}
+        </Text>
+        <Flex
+          justifyContent="center"
+          alignItems="center"
+          width="79px"
+          height="18px"
+          borderRadius="12px"
+          border="0.5px solid"
+          borderColor={
+            status === "Terverifikasi"
+              ? "#16A34A"
+              : status === "Ditolak"
+              ? "#DC2626"
+              : "#CA8A04"
+          }
+          background={
+            status === "Terverifikasi"
+              ? "#DCFCE7"
+              : status === "Ditolak"
+              ? "#FEE2E2"
+              : "#FFFAE6"
+          }
+          fontSize="10px"
           fontWeight="400"
-          color="gray.500"
-          lineHeight="140%"
-          margin="4px 0 4px 0"
+          color={
+            status === "Terverifikasi"
+              ? "#16A34A"
+              : status === "Ditolak"
+              ? "#DC2626"
+              : "#CA8A04"
+          }
         >
-          Kecamatan Kandanghaur, Kabupaten Indramayu, Jawa Barat
-        </Text>
-        <Text fontSize="10px" fontWeight="400" color="gray.300">
-          24/10/24
-        </Text>
-      </Flex>
-      <Flex
-        padding="12px"
-        borderRadius="8px"
-        border="1px solid"
-        borderColor="gray.300"
-        direction="column"
-        boxShadow="0px 1px 2px 0px rgba(0, 0, 0, 0.06), 0px 1px 3px 0px rgba(0, 0, 0, 0.10);"
-        cursor="pointer"
-      >
-        <Flex justifyContent="space-between">
-          <Text
-            fontSize="14px"
-            fontWeight="700"
-            lineHeight="140%"
-            width="221px"
-          >
-            Desa Putang
-          </Text>
-          <Flex
-            justifyContent="center"
-            alignItems="center"
-            width="79px"
-            height="18px"
-            borderRadius="12px"
-            border="0.5px solid"
-            borderColor="#16A34A"
-            background="#DCFCE7"
-            fontSize="10px"
-            fontWeight="400"
-            color="#16A34A"
-          >
-            Terverifikasi
-          </Flex>
+          {status}
         </Flex>
-        <Text
-          fontSize="12px"
-          fontWeight="400"
-          color="gray.500"
-          lineHeight="140%"
-          margin="4px 0 4px 0"
-        >
-          Kecamatan Kandanghaur, Kabupaten Indramayu, Jawa Barat
-        </Text>
-        <Text fontSize="10px" fontWeight="400" color="gray.300">
-          24/10/24
-        </Text>
       </Flex>
-      <Flex
-        padding="12px"
-        borderRadius="8px"
-        border="1px solid"
-        borderColor="gray.300"
-        direction="column"
-        boxShadow="0px 1px 2px 0px rgba(0, 0, 0, 0.06), 0px 1px 3px 0px rgba(0, 0, 0, 0.10);"
-        cursor="pointer"
+      <Text
+        fontSize="12px"
+        fontWeight="400"
+        color="gray.500"
+        lineHeight="140%"
+        margin="4px 0 4px 0"
       >
-        <Flex justifyContent="space-between">
-          <Text
-            fontSize="14px"
-            fontWeight="700"
-            lineHeight="140%"
-            width="221px"
+        {isExpanded ? description : getShortDescription(description, 14)}
+        {description.split(" ").length > 14 && (
+          <Button
+            variant="link"
+            color="brand.100"
+            fontWeight='400'
+            fontSize="12px"
+            ml="5px"
+            onClick={() => setIsExpanded(!isExpanded)}
           >
-            Desa Cikajang
-          </Text>
-          <Flex
-            justifyContent="center"
-            alignItems="center"
-            width="79px"
-            height="18px"
-            borderRadius="12px"
-            border="0.5px solid"
-            borderColor="#DC2626"
-            background="#FEE2E2"
-            fontSize="10px"
-            fontWeight="400"
-            color="#DC2626"
-          >
-            Ditolak
-          </Flex>
-        </Flex>
-        <Text
-          fontSize="12px"
-          fontWeight="400"
-          color="gray.500"
-          lineHeight="140%"
-          margin="4px 0 4px 0"
-        >
-          Kecamatan Kandanghaur, Kabupaten Indramayu, Jawa Barat
-        </Text>
-        <Text fontSize="10px" fontWeight="400" color="gray.300">
-          24/10/24
-        </Text>
-      </Flex>
-    </Stack>
+            {isExpanded ? "Sembunyikan" : "Lihat Semua"}
+          </Button>
+        )}
+      </Text>
+      <Text fontSize="10px" fontWeight="400" color="gray.400">
+        {date}
+      </Text>
+    </Flex>
   );
 };
 export default CardNotification;
--- src/pages/admin/verification/VerificationPage.tsx
+++ src/pages/admin/verification/VerificationPage.tsx
@@ -1,19 +1,101 @@
+import { Stack } from "@chakra-ui/react";
+import CardNotification from "Components/card/notification/CardNotification";
 import Container from "Components/container";
 import TopBar from "Components/topBar";
-import React from "react";
+import { collection, getDocs } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
 import { useNavigate, useParams } from "react-router-dom";
-import ListNotification from "./ListNotification";
+import { auth, firestore } from "../../../firebase/clientApp";
 import AdsPage from "../ads/AdsPage";
-
 
 const VerificationPage: React.FC = () => {
   const navigate = useNavigate();
-  const { category } = useParams();
+  const { category } = useParams<{ category: string }>();
+  const [user] = useAuthState(auth);
+  const [verifData, setVerifData] = useState<any[]>([]);
+
+  const formatTimestamp = (timestamp: {
+    seconds: number;
+    nanoseconds: number;
+  }) => {
+    if (!timestamp?.seconds) return "Invalid Date";
+    return new Date(timestamp.seconds * 1000).toLocaleDateString("id-ID", {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    });
+  };
+
+  const formatLocation = (lokasi: any) => {
+    if (!lokasi) return "No Location";
+    const kecamatan = lokasi.kecamatan?.label || "Unknown Subdistrict";
+    const kabupaten = lokasi.kabupatenKota?.label || "Unknown City";
+    const provinsi = lokasi.provinsi?.label || "Unknown Province";
+
+    return `KECAMATAN ${kecamatan}, ${kabupaten}, ${provinsi}`;
+  };
+
+
+
+  const fetchCategoryData = async (collectionName: string) => {
+    const docRef = collection(firestore, collectionName);
+    const docSnap = await getDocs(docRef);
+    return docSnap.docs.map((doc) => doc.data());
+  };
+
+  const fetchData = async () => {
+    switch (category) {
+      case "Verifikasi Desa":
+        return await fetchCategoryData("villages");
+      case "Verifikasi Innovator":
+        return await fetchCategoryData("innovators");
+      case "Verifikasi Tambah Innovasi":
+        return await fetchCategoryData("innovations");
+      default:
+        return [];
+    }
+  };
+
+  useEffect(() => {
+    const loadData = async () => {
+      const data = await fetchData();
+      console.log(data);
+      setVerifData(data || []);
+    };
+    loadData();
+  }, [category]);
 
   return (
     <Container page>
-      <TopBar title={category} onBack={() => navigate(-1)} />
-      {category === "Pembuatan Iklan" ? <AdsPage /> : <ListNotification />}
+      <TopBar title={category || "Verification"} onBack={() => navigate(-1)} />
+      <Stack padding="0 16px" gap={2}>
+        {category === "Pembuatan Iklan" ? (
+          <AdsPage />
+        ) : (
+          verifData.map((data, index) => {
+            const isVillageCategory = category === "Verifikasi Desa";
+            const description = isVillageCategory
+              ? formatLocation(data.lokasi)
+              : data.deskripsi || "No Description";
+
+            return (
+              <CardNotification
+                key={index}
+                title={
+                  data.namaDesa ||
+                  data.namaInovator ||
+                  data.namaInovasi ||
+                  "No Title"
+                }
+                status={data.status || "Unknown"}
+                date={formatTimestamp(data.createdAt)}
+                description={description}
+              />
+            );
+          })
+        )}
+      </Stack>
     </Container>
   );
 };
