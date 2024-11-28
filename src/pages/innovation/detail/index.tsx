6d3b8964a4eed050fb7f05e8b8f63d2743b98287
dinda <dindargsta@gmail.com>
2024-11-28 15:48:07 +0700

    change some styles for description2, text4,5

--- src/pages/innovation/detail/index.tsx
+++ src/pages/innovation/detail/index.tsx
@@ -1,4 +1,5 @@
-import { Button, Img } from "@chakra-ui/react";
+import { Button, Flex, Img,Text } from "@chakra-ui/react";
+import { Accordion, AccordionItem, AccordionButton, AccordionPanel, AccordionIcon, Box} from '@chakra-ui/react';
 import Check from "Assets/icons/check-circle.svg";
 import Container from "Components/container";
 import TopBar from "Components/topBar";
@@ -18,19 +19,23 @@ import {
   ChipContainer,
   ContentContainer,
   Description,
+  Description2,
   Icon,
   Label,
   Logo,
-  Text,
+  Text1,
   Text2,
   Text3,
+  Text4,
   Title,
 } from "./_detailStyle.ts";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../../../firebase/clientApp";
+import { FaCircle } from 'react-icons/fa';  // Import ikon elips
 
 function DetailInnovation() {
   const navigate = useNavigate();
+  const [isExpanded, setIsExpanded] = useState(false);
   const { id } = useParams();
   const { data: innovation } = useQuery<any>(
     "innovationById",
@@ -82,6 +87,13 @@ function DetailInnovation() {
   };
 
   const isUserCreator = user && user.uid === data.innovatorId; // Check if the current user is the creator
+  const truncateText = (text: string, wordLimit: number) => {
+    if (!text) return '';
+    const words = text.split(" ");
+    return words.length > wordLimit
+      ? words.slice(0, wordLimit).join(" ") 
+      : text;
+  };
 
   return (
     <Container page>
@@ -121,6 +133,9 @@ function DetailInnovation() {
         <div>
           <Title>{data.namaInovasi}</Title>
           <ChipContainer>
+            <Label>
+              masih diproduksi
+            </Label>
             <Label
               onClick={() =>
                 navigate(
@@ -132,7 +147,7 @@ function DetailInnovation() {
             >
               {data.kategori}
             </Label>
-            <Description>Sejak tahun {year}</Description>
+            <Description2>Sejak tahun {year}</Description2>
           </ChipContainer>
         </div>
         <ActionContainer
@@ -147,16 +162,150 @@ function DetailInnovation() {
           <Logo src={datainnovator.logo} alt="logo" />
           <div>
             <Text2>Inovator</Text2>
-            <Text>{datainnovator.namaInovator}</Text>
+            <Text1>{datainnovator.namaInovator}</Text1>
           </div>
         </ActionContainer>
+        
         <div>
-          <Text mb={16}>Deskripsi</Text>
-          <Description>{data.deskripsi}</Description>
+          <Text1 mb={5}>Deskripsi</Text1>
+          <Box fontSize="12px" fontWeight="400" color="#4B5563" >
+            {isExpanded ? (
+              // Tampilkan teks lengkap jika `isExpanded` true
+              <>
+                {data.deskripsi}
+                {data.deskripsi.split(" ").length > 20 && ( 
+                  <Text
+                    as="span"
+                    fontSize="12px"
+                    fontWeight="700"
+                    color="#347357"
+                    cursor="pointer"
+                    textDecoration="underline"
+                    onClick={() => setIsExpanded(!isExpanded)} // Toggle state
+                    >
+                      Lebih Sedikit
+                    </Text>
+                  )}
+              </>
+            ) : (
+              <>
+                {truncateText(data.deskripsi || '', 20)}
+                  {data.deskripsi && data.deskripsi.split(" ").length > 20 && ( // Tampilkan "Selengkapnya" jika lebih dari 20 kata
+                    <Text
+                      as="span"
+                      fontSize="12px"
+                      fontWeight="700"
+                      color="#347357"
+                      cursor="pointer"
+                      textDecoration="underline"
+                      onClick={() => setIsExpanded(!isExpanded)} // Toggle state
+                    >
+                      {" "}
+                      Selengkapnya
+                    </Text>
+                  )}
+                </>
+              )}
+          </Box>
         </div>
 
         <div>
-          <Text mb={16}>Perlu Disiapkan</Text>
+          <Text4 mt={-5} mb={5}>Model Bisnis Digital</Text4>
+        </div>
+
+        <div>
+          <Text4 mt={-5} mb={5}>Desa yang Menerapkan</Text4>
+        </div>
+
+        <div>
+          <Text4 mt={-5} mb={5}>Kisaran Harga</Text4>
+        </div>
+
+        <div>
+          <Text1 mt={-8} mb={10}>Manfaat</Text1>
+          <Flex>
+          <Accordion allowMultiple>
+            <Flex mb="12px">
+              <ActionContainer>
+                  <AccordionItem border="none">
+                  <h2>
+                    <AccordionButton border="none">
+                      <Flex w="100%" alignItems="center" justifyContent="space-between">
+                        <Box display="flex" alignItems="center" w="auto">
+                          <FaCircle style={{ marginRight: '10px', color: '#568A73' }} />
+                          <Text1 style={{ whiteSpace: 'nowrap' }}> {/* Menggunakan white-space: nowrap */}
+                            Manfaat 1: Meningkatkan Kesehatan
+                          </Text1>
+                        </Box>
+                        <AccordionIcon ml={8} color= '#568A73' />
+                      </Flex>
+                    </AccordionButton>
+                  </h2>
+                    <AccordionPanel pb={4}>
+                      <Description>
+                        Penjelasan mengenai manfaat pertama yang sangat penting untuk kesehatan tubuh, terutama dalam meningkatkan daya tahan tubuh dan metabolisme.
+                      </Description>
+                    </AccordionPanel>
+                  </AccordionItem>
+              </ActionContainer>
+            </Flex>
+
+            <Flex mb="12px">
+              <ActionContainer>
+                  <AccordionItem border="none">
+                  <h2>
+                    <AccordionButton border="none">
+                      <Flex w="100%" alignItems="center" justifyContent="space-between">
+                        <Box display="flex" alignItems="center" w="auto">
+                          <FaCircle style={{ marginRight: '10px', color: '#568A73' }} />
+                          <Text1 style={{ whiteSpace: 'nowrap' }}> {/* Menggunakan white-space: nowrap */}
+                            Manfaat 2: Meningkatkan Kesehatan
+                          </Text1>
+                        </Box>
+                        <AccordionIcon ml={8} color= '#568A73' />
+                      </Flex>
+                    </AccordionButton>
+                  </h2>
+                    <AccordionPanel pb={4}>
+                      <Description>
+                        Penjelasan mengenai manfaat pertama yang sangat penting untuk kesehatan tubuh, terutama dalam meningkatkan daya tahan tubuh dan metabolisme.
+                      </Description>
+                    </AccordionPanel>
+                  </AccordionItem>
+              </ActionContainer>
+            </Flex>
+
+            <Flex mb="12px">
+              <ActionContainer>
+                  <AccordionItem border="none">
+                  <h2>
+                    <AccordionButton border="none">
+                      <Flex w="100%" alignItems="center" justifyContent="space-between">
+                        <Box display="flex" alignItems="center" w="auto">
+                          <FaCircle style={{ marginRight: '10px', color: '#568A73' }} />
+                          <Text1 style={{ whiteSpace: 'nowrap' }}> {/* Menggunakan white-space: nowrap */}
+                            Manfaat 3: Meningkatkan Kesehatan
+                          </Text1>
+                        </Box>
+                        <AccordionIcon ml={8} color= '#568A73'/>
+                      </Flex>
+                    </AccordionButton>
+                  </h2>
+                    <AccordionPanel pb={4}>
+                      <Description>
+                        Penjelasan mengenai manfaat pertama yang sangat penting untuk kesehatan tubuh, terutama dalam meningkatkan daya tahan tubuh dan metabolisme.
+                      </Description>
+                    </AccordionPanel>
+                  </AccordionItem>
+              </ActionContainer>
+            </Flex>
+
+          </Accordion>
+          </Flex>
+        </div>
+
+        <div>
+          <Text1 mt={-8} mb={10}>Perlu Disiapkan</Text1>
           {Array.isArray(data.kebutuhan) && data.kebutuhan.length > 0 ? (
             data.kebutuhan.map((item, index) => (
               <BenefitContainer key={index}>
@@ -170,7 +319,7 @@ function DetailInnovation() {
         </div>
 
         <div>
-          <Text mb={16}>Desa yang Menerapkan </Text>
+          <Text1 mb={16}>Desa yang Menerapkan </Text1>
           <ActionContainer>
             <Text3>Belum tersedia</Text3>
           </ActionContainer>
