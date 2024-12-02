89c427e55e842327753d8a2ef989c0ee6c8da3ab
Adi Satria <adisatria@apps.ipb.ac.id>
2024-12-02 17:37:32 +0700

    Implement user profile update feature: add form for editing user details, validate input, and integrate with backend API

--- src/pages/innovator/detail/index.tsx
+++ src/pages/innovator/detail/index.tsx
@@ -1,44 +1,119 @@
-import { Flex, Icon, Stack, Text, useDisclosure, Box, DrawerCloseButton, Image, Drawer, DrawerBody, DrawerFooter, DrawerHeader, DrawerOverlay, DrawerContent, Button as ChakraButton, Link } from "@chakra-ui/react";
+import { ChevronRightIcon } from "@chakra-ui/icons";
+import {
+  Box,
+  Flex,
+  Icon,
+  Image,
+  Stack,
+  Text,
+  useDisclosure
+} from "@chakra-ui/react";
 import Button from "Components/button";
-import CardInnovation from "Components/card/innovation";
+import RejectionModal from "Components/confirmModal/RejectionModal";
 import Container from "Components/container";
+import ActionDrawer from "Components/drawer/ActionDrawer";
 import TopBar from "Components/topBar/index";
 import { paths } from "Consts/path";
-import { DocumentData, collection, doc, getDoc, getDocs, query, where } from "firebase/firestore";
+import {
+  DocumentData,
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  query,
+  updateDoc,
+  where,
+} from "firebase/firestore";
 import React, { useEffect, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
 import { FaWandMagicSparkles } from "react-icons/fa6";
 import { LuDot } from "react-icons/lu";
 import { TbPlant2 } from "react-icons/tb";
 import { generatePath, useNavigate, useParams } from "react-router-dom";
-import { firestore } from "../../../firebase/clientApp";
-import {
-  CardContainer,
-  Horizontal,
-} from "../../../pages/home/components/innovator/_innovatorStyle";
+import { auth, firestore } from "../../../firebase/clientApp";
+import ButtonPengajuan from "../components/hero/ButtonPengajuan"; // Impor tombol
+import InnovationPreview from "../components/hero/innovations";
 import {
   Background,
   ContentContainer,
-  Description,
   Label,
   Logo,
-  Title
+  Title,
 } from "./_detailStyle";
-import { ChevronRightIcon } from "@chakra-ui/icons"; 
-import { FaWhatsapp, FaInstagram, FaGlobe } from "react-icons/fa";
-import InnovationPreview from "../components/hero/innovations";
-import ButtonPengajuan from "../components/hero/ButtonPengajuan"; // Impor tombol
 
 const DetailInnovator: React.FC = () => {
   const [isExpanded, setIsExpanded] = useState(false);
   const navigate = useNavigate();
   const { id } = useParams(); // Ensure TypeScript knows id is a string
-  const { isOpen, onOpen, onClose } = useDisclosure()
+  const { isOpen, onOpen, onClose } = useDisclosure();
   const [innovatorData, setInnovatorData] = useState<DocumentData | null>(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
   const [innovations, setInnovations] = useState<DocumentData[]>([]);
   const [villages, setVillages] = useState<DocumentData[]>([]); // Add state for villages
+  const [admin, setAdmin] = useState(false);
+  const [userLogin] = useAuthState(auth);
+  const [openModal, setOpenModal] = useState(false);
+  const [modalInput, setModalInput] = useState("");
 
+  const handleVerify = async () => {
+    setLoading(true);
+    try {
+      if (!id) {
+        setError("Invalid innovator ID.");
+        setLoading(false);
+        return;
+      }
+      const docRef = doc(firestore, "innovators", id);
+      await updateDoc(docRef, {
+        status: "Terverifikasi",
+      });
+      setInnovatorData((prev) => ({ ...prev, status: "Terverifikasi" }));
+    } catch (error) {
+      console.error("Error verifying innovator:", error);
+      setError("Error verifying innovator.");
+    }
+    setLoading(false);
+    onClose();
+  };
+
+  const handleReject = async () => {
+    try {
+      if (!id) {
+        setError("Invalid innovator ID.");
+        setLoading(false);
+        return;
+      }
+      const docRef = doc(firestore, "innovators", id);
+      await updateDoc(docRef, {
+        status: "Ditolak",
+        catatanAdmin: modalInput,
+      });
+      setInnovatorData((prev) => ({
+        ...prev,
+        status: "Ditolak",
+        catatanAdmin: modalInput,
+      }));
+    } catch (error) {
+      console.error("Error rejecting innovator:", error);
+      setError("Error rejecting innovator.");
+    }
+    setLoading(false);
+    setOpenModal(false);
+  };
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      if (userLogin?.uid) {
+        const userRef = doc(firestore, "users", userLogin.uid);
+        const userDoc = await getDoc(userRef);
+        if (userDoc.exists()) {
+          setAdmin(userDoc.data().role === "admin");
+        }
+      }
+    }
+    fetchUser();
+  })
   // Fetch innovator data
   useEffect(() => {
     if (!id) {
@@ -98,8 +173,11 @@ const DetailInnovator: React.FC = () => {
       {
         id: "village1",
         namaDesa: "Desa Puntang",
-        inovasiDiterapkan: ["Pakan Otomatis (eFeeder)", "Lapak Ikan (eFisheryFeed)"],
-        logo: "https://via.placeholder.com/50"
+        inovasiDiterapkan: [
+          "Pakan Otomatis (eFeeder)",
+          "Lapak Ikan (eFisheryFeed)",
+        ],
+        logo: "https://via.placeholder.com/50",
       },
     ];
     setVillages(dummyVillages);
@@ -123,13 +201,10 @@ const DetailInnovator: React.FC = () => {
       ? words.slice(0, wordLimit).join(" ") + "..."
       : text;
   };
-  
+
   return (
     <Container page>
-      <TopBar 
-        title="Profil Inovator"  
-        onBack={() => navigate(-1)} 
-        />
+      <TopBar title="Profil Inovator" onBack={() => navigate(-1)} />
       <Flex position="relative">
         <Background src={innovatorData.header} alt="header" />
         <Logo src={innovatorData.logo} alt="logo" mx={16} my={-40} />
@@ -146,7 +221,7 @@ const DetailInnovator: React.FC = () => {
         </Box>
       </Flex>
       <ContentContainer>
-        <Stack gap={3} mt={4} >
+        <Stack gap={3} mt={4}>
           <Title>{innovatorData.namaInovator}</Title>
           <Label>{innovatorData.kategori}</Label>
           <Flex direction="row" gap={3} mt={0} alignItems="center">
@@ -173,7 +248,9 @@ const DetailInnovator: React.FC = () => {
                 alignItems="flex-start"
                 paddingBottom="12px"
               >
-                <Box color="#4B5563" fontSize="12px" minWidth="110px">Nomor WhatsApp</Box>
+                <Box color="#4B5563" fontSize="12px" minWidth="110px">
+                  Nomor WhatsApp
+                </Box>
                 <Text
                   maxW="200px"
                   whiteSpace="normal"
@@ -189,7 +266,9 @@ const DetailInnovator: React.FC = () => {
                 alignItems="flex-start"
                 paddingBottom="12px"
               >
-                <Box color="#4B5563" fontSize="12px" minWidth="110px">Link Instagram</Box>
+                <Box color="#4B5563" fontSize="12px" minWidth="110px">
+                  Link Instagram
+                </Box>
                 <Text
                   maxW="222px"
                   whiteSpace="normal"
@@ -205,7 +284,9 @@ const DetailInnovator: React.FC = () => {
                 alignItems="flex-start"
                 paddingBottom="12px"
               >
-                <Box color="#4B5563" fontSize="12px" minWidth="110px">Link Website</Box>
+                <Box color="#4B5563" fontSize="12px" minWidth="110px">
+                  Link Website
+                </Box>
                 <Text
                   maxW="222px"
                   whiteSpace="normal"
@@ -217,26 +298,16 @@ const DetailInnovator: React.FC = () => {
               </Flex>
             </Flex>
 
-            <Flex direction="row" alignItems="center" >
-              <Text
-                fontSize="12px"
-                fontWeight="700"
-                color="#4B5563"
-                mr={2}
-              >
+            <Flex direction="row" alignItems="center">
+              <Text fontSize="12px" fontWeight="700" color="#4B5563" mr={2}>
                 Model bisnis digital:
               </Text>
-              <Text
-                fontSize="12px"
-                fontWeight="400"
-                color="#4B5563"
-                flex="1"
-              >
+              <Text fontSize="12px" fontWeight="400" color="#4B5563" flex="1">
                 {innovatorData.modelBisnis}
               </Text>
             </Flex>
 
-            <Box fontSize="12px" fontWeight="400" color="#4B5563" >
+            <Box fontSize="12px" fontWeight="400" color="#4B5563">
               {isExpanded ? (
                 // Tampilkan teks lengkap jika `isExpanded` true
                 <>
@@ -298,7 +369,9 @@ const DetailInnovator: React.FC = () => {
               backgroundColor="white"
               borderColor="gray.200"
               onClick={() =>
-                navigate(generatePath(paths.DETAIL_VILLAGE_PAGE, { id: village.id }))
+                navigate(
+                  generatePath(paths.DETAIL_VILLAGE_PAGE, { id: village.id })
+                )
               }
             >
               <Flex alignItems="center" mb={3}>
@@ -309,8 +382,10 @@ const DetailInnovator: React.FC = () => {
                   borderRadius="full"
                   mr={4}
                 />
-                <Text fontSize="12px" fontWeight="600">{village.namaDesa}</Text>
-                <ChevronRightIcon color="gray.500" ml="auto"/>
+                <Text fontSize="12px" fontWeight="600">
+                  {village.namaDesa}
+                </Text>
+                <ChevronRightIcon color="gray.500" ml="auto" />
               </Flex>
               {/* Menambahkan Border Pembatas Di Atas "Inovasi Diterapkan" */}
               <Box borderTop="1px" borderColor="gray.300" pt={3} mt={3}></Box>
@@ -339,102 +414,24 @@ const DetailInnovator: React.FC = () => {
         <Button mt={-3} size="m" fullWidth type="submit" onClick={onOpen}>
           Kontak Inovator
         </Button>
-        </ContentContainer>
-        <Drawer
-          isOpen={isOpen}
-          placement="bottom"
-          onClose={onClose}
-        >
-          <DrawerOverlay />
-          <DrawerContent
-            sx={{
-              borderTopRadius: "16px", // Radius untuk tampilan yang smooth
-              width: "100%", // Lebar drawer penuh
-              maxWidth: "480px", // Batas maksimal untuk mobile
-              margin: "0 auto", // Pusatkan drawer
-              bg: "white", // Warna latar belakang putih
-            }}
-          >
-            <DrawerHeader
-              sx={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                fontSize: "16px",
-                fontWeight: "700",
-                color: "#1F2937",
-                padding: "16px",
-              }}
-            >
-              Kontak Inovator
-            </DrawerHeader>
-            <DrawerCloseButton mt={2} />
-            <DrawerBody padding="16px">
-              <Text fontSize="12px" fontWeight="400" color="#4B5563" mb={4} textAlign="center">
-                Terapkan produk inovasi desa digital dengan cara menghubungi innovator melalui saluran di bawah ini:
-              </Text>
-              <Stack spacing={4}>
-                {/* WhatsApp */}
-                <Flex
-                  alignItems="center"
-                  padding="12px"
-                  borderWidth="1px"
-                  borderRadius="8px"
-                  cursor="pointer"
-                  transition="all 0.3s ease"
-                  _hover={{
-                    bg: "green.50", // Warna saat hover
-                  }}
-                >
-                  <Icon as={FaWhatsapp} boxSize={6} color="green.500" mr={4} />
-                  <Text fontSize="14px" fontWeight="500" flex="1" color="#1F2937">
-                    WhatsApp
-                  </Text>
-                  <ChevronRightIcon color="#1F2937" />
-                </Flex>
-
-                {/* Instagram */}
-                <Flex
-                  alignItems="center"
-                  padding="12px"
-                  borderWidth="1px"
-                  borderRadius="8px"
-                  cursor="pointer"
-                  transition="all 0.3s ease"
-                  _hover={{
-                    bg: "blue.50", // Warna saat hover
-                  }}
-                >
-                  <Icon as={FaInstagram} boxSize={6} color="blue.500" mr={4} />
-                  <Text fontSize="14px" fontWeight="500" flex="1" color="#1F2937">
-                    Instagram
-                  </Text>
-                  <ChevronRightIcon color="#1F2937" />
-                </Flex>
-
-                {/* Website */}
-                <Flex
-                  alignItems="center"
-                  padding="12px"
-                  borderWidth="1px"
-                  borderRadius="8px"
-                  cursor="pointer"
-                  transition="all 0.3s ease"
-                  _hover={{
-                    bg: "teal.50", // Warna saat hover
-                  }}
-                >
-                  <Icon as={FaGlobe} boxSize={6} color="teal.500" mr={4} />
-                  <Text fontSize="14px" fontWeight="500" flex="1" color="#1F2937">
-                    Website
-                  </Text>
-                  <ChevronRightIcon color="#1F2937" />
-                </Flex>
-              </Stack>
-            </DrawerBody>
-          </DrawerContent>
-        </Drawer>
-
+        <RejectionModal
+          isOpen={openModal}
+          onClose={() => setOpenModal(false)}
+          onConfirm={handleReject}
+          message={modalInput}
+          setMessage={setModalInput}
+          loading={loading}
+        />
+      </ContentContainer>
+      <ActionDrawer
+        isOpen={isOpen}
+        onClose={onClose}
+        isAdmin={admin}
+        onVerify={handleVerify}
+        setOpenModal={setOpenModal}
+        loading={loading}
+        role="inovator"
+      />
     </Container>
   );
 };
